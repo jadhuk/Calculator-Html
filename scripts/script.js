167c3097852c7d418698e37607e0d17baa16287c
jadhuk herlambang <jadhukherlambang@jadhuks-MacBook-Air.local>
2023-02-02 12:51:48 +0700

    add calculator

---
+++ scripts/script.js
@@ -0,0 +1,88 @@
+const moonIcon = "assets/MoonIcon.svg";
+const res = document.getElementById("result");
+const toast = document.getElementById("toast");
+
+function calculate(value) {
+  const calculatedValue = eval(value || null);
+  let decimalAdded = false;
+
+  if (isNaN(calculatedValue)) {
+    res.value = "Can't divide 0 with 0";
+    setTimeout(() => {
+      res.value = "";
+    }, 1300);
+  } else {
+    res.value = calculatedValue;
+  }
+}
+
+if (key === "." && decimalAdded) {
+  return;
+}
+
+if (operators.indexOf(key) !== -1) {
+  decimalAdded = false;
+}
+
+function liveScreen(enteredValue) {
+  if (!res.value) {
+    res.value = "";
+  }
+  res.value += enteredValue;
+}
+
+document.addEventListener("keydown", keyboardInputHandler);
+
+function keyboardInputHandler(e) {
+  e.preventDefault();
+
+  if (e.key === "0") {
+    res.value += "0";
+  } else if (e.key === "1") {
+    res.value += "1";
+  } else if (e.key === "2") {
+    res.value += "2";
+  } else if (e.key === "3") {
+    res.value += "3";
+  } else if (e.key === "4") {
+    res.value += "4";
+  } else if (e.key === "5") {
+    res.value += "5";
+  } else if (e.key === "6") {
+    res.value += "6";
+  } else if (e.key === "7") {
+    res.value += "7";
+  } else if (e.key === "7") {
+    res.value += "7";
+  } else if (e.key === "8") {
+    res.value += "8";
+  } else if (e.key === "9") {
+    res.value += "9";
+  }
+
+  if (e.key === "+") {
+    res.value += "+";
+  } else if (e.key === "-") {
+    res.value += "-";
+  } else if (e.key === "*") {
+    res.value += "*";
+  } else if (e.key === "/") {
+    res.value += "/";
+  } else if (e.key === "AC") {
+    const resultInput = res.value;
+    res.value = resultInput.substring(0, res.value.length - 1);
+  }
+
+  if (e.key === "." && (decimalAdded = true)) {
+    res.value += ".";
+  }
+
+  if (e.key === "Enter") {
+    calculate(result.value);
+  }
+
+  if (e.key === "Backspace") {
+    const resultInput = res.value;
+    res.value = resultInput.substring(0, res.value.length - 1);
+  }
+}
